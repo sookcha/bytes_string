ce7e7e66d2758fd3644fbcac0ed5b34af1e68e06
Hoseong Son <lsookcha@me.com>
2017-12-20 00:00:20 +0900

    Add integer to hex array

--- __init__.py
+++ __init__.py
@@ -1,3 +1,28 @@
-class ByteString:
-    def __init__(self):
+# -*- coding: utf-8 -*-
+import array
+
+
+class BytesString:
+    def __init__(self, size):
+        self.byte_arr = bytearray(size)
+        self.bytes_str_arr = ['00' for x in range(size)]
+
+    def add_string(self, text, index):
         pass
+
+    def add_integer(self, number, index):
+        text = hex(number).replace("0x", "")
+        splited_arr = self.split_by_n(text, 2)
+        print(list(splited_arr))
+
+    def position(self, new_position):
+        self.byte_arr.zfill(new_position)
+
+    def get_bytearray(self):
+        return self.byte_arr
+
+    def split_by_n(self, seq, n):
+        while seq:
+            yield seq[:n]
+            seq = seq[n:]
+            seq = ("0%s" % seq) if len(seq) == 1 else seq
