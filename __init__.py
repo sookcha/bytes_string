08eca68d675f5ee6b54d6e80e433bf90d5447179
Hoseong Son <lsookcha@me.com>
2017-12-20 09:25:39 +0900

    Add integer, string capabilities

--- __init__.py
+++ __init__.py
@@ -1,25 +1,25 @@
 # -*- coding: utf-8 -*-
-import array
 
 
 class BytesString:
     def __init__(self, size):
-        self.byte_arr = bytearray(size)
         self.bytes_str_arr = ['00' for x in range(size)]
 
-    def add_string(self, text, index):
-        pass
+    def add_string(self, text, index, encoding):
+        self.add_to_array(text.encode(encoding).hex(), index)
 
     def add_integer(self, number, index):
         text = hex(number).replace("0x", "")
-        splited_arr = self.split_by_n(text, 2)
-        print(list(splited_arr))
-
-    def position(self, new_position):
-        self.byte_arr.zfill(new_position)
+        self.add_to_array(text, index)
 
     def get_bytearray(self):
-        return self.byte_arr
+        return self.bytes_str_arr
+
+    def add_to_array(self, text, index):
+        splited_arr = self.split_by_n(text, 2)
+        for element in reversed(list(splited_arr)):
+            self.bytes_str_arr[index - 1] = element
+            index -= 1
 
     def split_by_n(self, seq, n):
         while seq:
