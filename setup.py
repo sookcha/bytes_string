347fff69aee9be6a40fd6e0f64bee78ca08d70ec
Hoseong Son <lsookcha@me.com>
2017-12-20 11:09:45 +0900

    Setup for pypi

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name="bytes_string",
+      version="1.0",
+      description="Manipulate bytestring on Python, without hassle.",
+      author="Hoseong Son",
+      author_email="me@sookcha.com",
+      url="https://github.com/sookcha/bytes_string",
+      packages=["bytes_string"],
+      )
